afb5b78b5107ba2ed84cd265e57c38a02ba260e8
utsav.h.talati <utsav.h.talati@vanderbilt.edu>
2023-11-21 21:56:44 -0600

    UI changes/buttons

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 # Sample project data (you might use a database in a real application)
 projects = [
-    {"id": 1, "name": "Project A", "tasks": ["Task 1", "Task 2"]},
-    {"id": 2, "name": "Project B", "tasks": ["Task 3", "Task 4"]},
+    {"id": 1, "name": "Project A", "tasks": [{"id": 1, "description": "Task 1", "complete": False}, {"id": 2, "description": "Task 2", "complete": False}]}
+    
 ]
 
 @app.route('/')
@@ -26,7 +26,29 @@ def add_task(project_id):
     project = next((p for p in projects if p["id"] == project_id), None)
     
     if project and task_description:
-        project["tasks"].append(task_description)
+        new_task = {"id": len(project['tasks']) + 1, "description": task_description, "complete": False}
+        project["tasks"].append(new_task)
+    
+    return redirect(url_for('project', project_id=project_id))
+
+@app.route('/complete_task/<int:project_id>/<int:task_id>')
+def complete_task(project_id, task_id):
+    project = next((p for p in projects if p["id"] == project_id), None)
+    
+    if project:
+        task = next((t for t in project['tasks'] if t["id"] == task_id), None)
+        
+        if task:
+            task["complete"] = True
+    
+    return redirect(url_for('project', project_id=project_id))
+
+@app.route('/delete_task/<int:project_id>/<int:task_id>')
+def delete_task(project_id, task_id):
+    project = next((p for p in projects if p["id"] == project_id), None)
+    
+    if project:
+        project["tasks"] = [t for t in project['tasks'] if t["id"] != task_id]
     
     return redirect(url_for('project', project_id=project_id))
 
